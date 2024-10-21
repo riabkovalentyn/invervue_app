d22c857e0ca311ac675578960957fd1c90f19b50
Valentyn Riabko <rabkovalen@gmail.com>
2024-10-21 22:27:28 +0100

    uptd index

--- invervue_app/src/store/index.ts
+++ invervue_app/src/store/index.ts
@@ -1,14 +1,31 @@
 import { createStore } from 'vuex'
+import { FormData } from '@/types/types'
+
+
+export interface State {
+  currentStep: number;
+  totalSteps: number;
+}
 
 export default createStore({
-  state: {
+  state:{
+    formData: {} as FormData,
+    currentStep: 1,
+    totalSteps: 3,
+  },
+    mutations: {
+    updateForm(state, formData: FormData) {
+      state.formData = {...state.formData, ...formData};
+    },
+    nextStep(state) {
+      if(state.currentStep < state.totalSteps) {
+        state.currentStep++;
+      }
+    },
   },
   getters: {
+    isLastStep: (state) => {
+      return state.currentStep === state.totalSteps;
+    },
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+});
